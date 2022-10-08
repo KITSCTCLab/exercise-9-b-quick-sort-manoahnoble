3e6be82598f0e2fffff1d34be97c840a54380e86
manoahnoble <110010556+manoahnoble@users.noreply.github.com>
2022-10-08 22:24:12 +0530

    intial commit

--- Main.py
+++ Main.py
@@ -1,8 +1,19 @@
 from typing import List
 
-def quick_sort(data, low, high) -> List[int]:
-    # Write code here
+def partition(arr, l, r):
+    p, i = l, l
+    while i < r:
+        if arr[i] <= arr[p]:
+            arr[i], arr[p] = arr[p], arr[i]
+            p += 1
+        i += 1
+    return p
 
+def quick_sort(data, l, r) -> List[int]:
+    p = partition(data, l, r)
+    quick_sort(data, l, p)
+    quick_sort(data, p, r)
+    return data
 
 input_data = input()
 data = []
